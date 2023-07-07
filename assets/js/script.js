5eb499599b3126db5a371f5506c84735bf38d5d8
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-07-07 16:12:27 -0500

    added ability to go to next question and endquiz fucntion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,81 @@
 // Define an array of quiz questions
 const quizQuestions = [
     {
-      question: "What is Does CSS stand for?",
-      options: ["Cascading Style Sheets", "Concave Sytle Sheets", "Cascading Sheet Styles", "All the above"],
-      answer: 0
+        question: "What is Does CSS stand for?",
+        options: ["Cascading Style Sheets", "Concave Sytle Sheets", "Cascading Sheet Styles", "All the above"],
+        answer: 0
     },
-    {question: "What is the correct HTML for referring to an external style sheet?",
-      options: ["<stylesheet>mystyle.css</stylesheet>", "<link rel='stylesheet' type='text/css' href='mystyle.css'>", "<style src='mystyle.css'>", "<link href='mystyle.css'>"],
-      answer: 1
-    } ,  
+    {
+        question: "What is the correct HTML for referring to an external style sheet?",
+        options: ["<stylesheet>mystyle.css</stylesheet>", "<link rel='stylesheet' type='text/css' href='mystyle.css'>", "<style src='mystyle.css'>", "<link href='mystyle.css'>"],
+        answer: 1
+    },
+    {
+        question: "Where in an HTML document is the correct place to refer to an external style sheet?",
+        options: ["In the <head> section", "In the <body> section", "At the end of the document", "In the <footer> section"],
+        answer: 0
+    }
     // Add more questions here
-  ];
-    // Define variables
-    var startButton = document.getElementById("start-button");
-    var questionDisplay = document.getElementById("question");
-    var answerOptions = document.getElementById("answer");
-    var resultDisplay = document.getElementById("result");
-    let currentQuestionIndex = 0;
-    // when i click the start button the first question should appear
-    startButton.addEventListener("click", function() {
+];
+// Define variables
+var startButton = document.getElementById("start-button");
+var questionDisplay = document.getElementById("question");
+var answerOptions = document.getElementById("answer");
+var resultDisplay = document.getElementById("result");
+let currentQuestionIndex = 0;
+// when i click the start button the first question should appear
+startButton.addEventListener("click", function () {
     //    hide the start button
-        startButton.style.display = "none";
-        // show the first question
+    startButton.style.display = "none";
+    // show the first question
 
-        showQuestion(0);
-    });
+    showQuestion(0);
+});
 //    creates funtion to show the question
-    function showQuestion(questionIndex) {
-        var currentQuestion = quizQuestions[questionIndex];
-        questionDisplay.textContent = currentQuestion.question;
-// clear answer options
-answerOptions.innerHTML = "";
-// loop through the answer options and create a button for each one
+function showQuestion(questionIndex) {
+    var currentQuestion = quizQuestions[questionIndex];
+    questionDisplay.textContent = currentQuestion.question;
+    // clear answer options
+    answerOptions.innerHTML = "";
+    // loop through the answer options and create a button for each one
 
-for (let i = 0; i < currentQuestion.options.length; i++) {
-    var option = document.createElement("button");
-    option.textContent = currentQuestion.options[i];
-    option.addEventListener("click", selectAnswer);
-    answerOptions.appendChild(option);
-}}
+    for (let i = 0; i < currentQuestion.options.length; i++) {
+        var option = document.createElement("button");
+        option.textContent = currentQuestion.options[i];
+        option.addEventListener("click", selectAnswer);
+        answerOptions.appendChild(option);
+    }
+}
 // function to select answer
-    function selectAnswer(event) {
-        const selectedOption = event.target;
-        const selectedAnswer = selectedOption.textContent;
-        const currentQuestion = quizQuestions[currentQuestionIndex];
+function selectAnswer(event) {
+    const selectedOption = event.target;
+    const selectedAnswer = selectedOption.textContent;
+    const currentQuestion = quizQuestions[currentQuestionIndex];
     // logic to see if answer is correct
     if (selectedAnswer === currentQuestion.options[currentQuestion.answer]) {
-       resultDisplay.textContent = "Correct!";
+        resultDisplay.textContent = "Correct!";
     }
     else {
         resultDisplay.textContent = "Incorrect!";
     }
+
+    currentQuestionIndex++;
+    //  checks to see if quiz is over and shows next question
     
-    currentQuestionIndex++; 
-    // function to show next question and checks to see if quiz is over
-    function nextQuestion() {
-        if (currentQuestionIndex < quizQuestions.length) {
-            endQuiz();
-        }
-        else {
-            showQuestion()
-    }}}
+    if (currentQuestionIndex < quizQuestions.length) {
+        showQuestion(currentQuestionIndex);
+       
+    }
+    else {
+        endQuiz();        
+    }
+}
+
+// function to end quiz
+function endQuiz() {
+    // hide question
+    questionDisplay.style.display = "none";
+    // hide answer options
+    answerOptions.style.display = "none";
+ 
+}
